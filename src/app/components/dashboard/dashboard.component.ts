d884df6769f1bb0108115010a8f3b1c7968a378b
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-04-07 23:44:24 -0400

    spinner and get data

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -10,16 +10,26 @@ export class DashboardComponent implements OnInit {
 
   imgUrl = 'https://cdn-icons-png.flaticon.com/512/1116/1116453.png';
   ciudad = '';
+  loading = false;
+  temperatura = 0;
+  humedad = 0;
+  clima = '';
+  query = false;
   constructor(private _climaService: ClimaService) { }
 
   ngOnInit(): void {
   }
 
   obtenerClima() {
-    console.log(this.ciudad);
-
+    this.loading = true;
+    this.query = false;
     this._climaService.getClima(this.ciudad).subscribe(data => {
       console.log(data);
+      this.query = true;
+      this.loading = false;
+      this.temperatura = data.main.temp - 273;
+      this.clima = data.weather[0].main;
+      this.humedad = data.main.humidity
     }, error => console.log(error));
   }
 
