d4f89604a8df8a268d6d43dd8968a3f63ac7e094
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-04-07 23:00:32 -0400

    use httpClient

--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +8,12 @@ import { Injectable } from '@angular/core';
 export class ClimaService {
 
   key = 'c80258f786fa6adcb91d2888be17a0ff';
+  url = 'https://api.openweathermap.org/data/2.5/weather?appid=';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getClima(ciudad: string): Observable<any> {
+    const httpUrl = this.url + this.key + '&q=' + ciudad;
+    return this.httpClient.get(this.url);
+  }
 }
